ee8e9ce9a2be9fc9b584cbf607d9d3df8d133866
MatthieuCrouzet <mattcrouzet@hotmail.fr>
2015-06-05 11:00:05 +0200

    Ajout et implémentation de la classe Position

---
+++ LightBot/src/Position.java
@@ -0,0 +1,54 @@
+
+public class Position {
+	
+	int l, c;
+	
+	/**
+	 * constructeurs
+	 */
+	public Position(){
+		l = 0;
+		c = 0;
+	}
+	public Position(int x, int y){
+		l = x;
+		c = y;
+	}
+	public Position(Position p){
+		c = p.getC();
+		l = p.getL();
+	}
+	
+	/**
+	 * renvoie la position horizontale du Bot
+	 * @return la valeur de l'attribut x
+	 */
+	public int getL(){
+		return l;
+	}
+	
+	/**
+	 * renvoie la position verticale du Bot
+	 * @return la valeur de l'attribut y
+	 */
+	public int getC(){
+		return c;
+	}
+	
+	/**
+	 * met à jour la position du Bot
+	 * @param x : nouvelle position horizontale 
+	 */
+	public void setC(int new_c){
+		c = new_c;
+	}
+	
+	/**
+	 * met à jour la position du Bot
+	 * @param y : nouvelle position verticale 
+	 */
+	public void setL(int new_l){
+		l = new_l;
+	}
+
+}
