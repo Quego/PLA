cc584d4998cfbd58fdaf7bf804611d24cfd085c8
MatthieuCrouzet <mattcrouzet@hotmail.fr>
2015-06-05 15:05:43 +0200

    Correction Avancer (liberation de la cellule precedente et occupation de la nouvelle cellule)

--- LightBot/src/Cellule.java
+++ LightBot/src/Cellule.java
@@ -17,21 +17,21 @@ public class Cellule {
 		this.hauteur = h;
 		this.objet = Objet.RIEN;
 		this.couleur = Couleur.BLANC;
-		this.occupe = false;
+		this.occupee = false;
 	}
 	
 	public Cellule(Couleur c) {
 		this.hauteur = 0;
 		this.objet = Objet.RIEN;
 		this.couleur = c;
-		this.occupe = false;
+		this.occupee = false;
 	}
 	
 	public Cellule(int h, Objet ob, Couleur c, boolean oc) {
 		this.hauteur = h;
 		this.objet = ob;
 		this.couleur = c;
-		this.occupe = oc;
+		this.occupee = oc;
 	}
 	
 	public int getHauteur() {return this.hauteur;}
@@ -43,14 +43,14 @@ public class Cellule {
 	public Couleur getCouleur() {return this.couleur;}
 	public void setCouleur(String c) {this.couleur = Couleur.valueOf(c);}
 	
-	public boolean getOccupe() {return this.occupe;}
-	public void setOccupe(boolean o) {this.occupe = o;}
+	public boolean getOccupee() {return this.occupee;}
+	public void setOccupee(boolean o) {this.occupee = o;}
 	
 	public String toString() {
 		return "hauteur = " + this.getHauteur() + "\n" +
 			   "objet = " + this.objet + "\n" + 
 			   "couleur = " + this.couleur + "\n" +
-			   "occupe = " + this.occupe + "\n";
+			   "occupee = " + this.occupe + "\n";
 	}
 	
 }
