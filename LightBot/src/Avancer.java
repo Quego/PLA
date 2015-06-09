1fe5e8be591ba334aff034b0c9b9b11c35fd76df
Kxanderelyandouzi <eykxander@gmail.com>
2015-06-09 11:24:43 +0200

    Ajout condition dans la classe Avancer

--- LightBot/src/Avancer.java
+++ LightBot/src/Avancer.java
@@ -13,8 +13,8 @@ public class Avancer implements Action {
 		if (!destination.estDansMap()) throw new Ensure("Le robot sort de la Map");
 		Cellule ma_cellule = Controleur.getMap().getCellule(p);
 		Cellule cellule_dest = Controleur.getMap().getCellule(destination);
-		accessible = ((ma_cellule.getHauteur() == cellule_dest.getHauteur()) && (!cellule_dest.getOccupee()));		
-		if (!accessible) throw new Ensure("La cellule de destination n'est pas à la meme hauteur");
+		accessible = ((ma_cellule.getHauteur() == cellule_dest.getHauteur()) && (cellule_dest.getCouleur() != Couleur.GRIS) && cellule_dest.getOccupee());		
+		if (!accessible) throw new Ensure("La cellule de destination n'est pas accessible");
 		Controleur.getBotActif().setPosition(destination);
 		ma_cellule.setOccupee(false); cellule_dest.setOccupee(true);
 	}
