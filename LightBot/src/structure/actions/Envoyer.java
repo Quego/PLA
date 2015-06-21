01407cc5b99baefcaa00e9b398e16b87aca6c65f
KingBabou <tib-noug@hotmail.fr>
2015-06-21 21:30:42 +0200

    Révision de Envoyer quand la liste est vide

--- LightBot/src/structure/actions/Envoyer.java
+++ LightBot/src/structure/actions/Envoyer.java
@@ -6,8 +6,11 @@ public class Envoyer implements Action {
 
 	public int executer() {
 		int num = Controleur.numeroBot();
-		Controleur.getMapCourrante().getBot((num+1)%2).getObjets().add(0, Controleur.getMapCourrante().getBot(num).getObjets().get(0));
-		Controleur.getMapCourrante().getBot(num).getObjets().remove(0);
+		if (!Controleur.getMapCourrante().getBot(num).getObjets().isEmpty()) {
+			Controleur.getMapCourrante().getBot((num+1)%2).getObjets().add(0, Controleur.getMapCourrante().getBot(num).getObjets().get(0));
+			Controleur.getMapCourrante().getBot(num).getObjets().remove(0);
+			return 1;
+		}
 		return 0;
 	}
 
